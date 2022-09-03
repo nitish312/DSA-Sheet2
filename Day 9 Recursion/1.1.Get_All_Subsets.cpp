3e632a0a8c9f45c5332dca825d4432e6c440b96c
nitish312 <nitishchintakindi@gmail.com>
2022-09-03 12:43:02 +0530

    recursion

---
+++ Day 9 Recursion/1.1.Get_All_Subsets.cpp
@@ -0,0 +1,81 @@
+// Given an integer array nums of unique elements, return all possible subsets (the power set).
+// The solution set must not contain duplicate subsets. Return the solution in any order.
+#include<iostream>
+#include<vector>
+using namespace std;
+
+// Recursive ? O(2^N) : O(2^N)
+void helper(int ind, vector<int>& nums, vector<int>& subset, vector<vector<int>>& ans){
+
+    if(ind == nums.size()){
+
+        ans.push_back(subset);
+        return;
+    }
+    else{
+
+        helper(ind + 1, nums, subset, ans); // ith elem not picked
+
+        subset.push_back(nums[ind]); // ith elem picked
+        helper(ind + 1, nums, subset, ans);
+
+        subset.pop_back(); // backtracking
+    }
+}
+
+vector<vector<int>> subsets2(vector<int>& nums){
+
+    vector<vector<int>> ans;
+
+    vector<int> subset;
+
+    helper(0, nums, subset, ans);
+
+    return ans;
+}
+
+
+
+// Iterative ? O(2^N) : O(2^N)
+vector<vector<int>> subsets(vector<int>& nums){
+
+    vector<vector<int>> ans;
+
+    int n = nums.size();
+
+    ans.push_back({});
+
+    for(int i=0; i<n; i++){
+
+        int sz = ans.size();
+
+        for(int j=0; j<sz; j++){
+
+            vector<int> vec = ans[j];
+            vec.push_back(nums[i]);
+
+            ans.push_back(vec);
+        }
+    }
+
+    return ans;
+}
+
+int main(){
+
+    vector<int> nums = {1,2,3};
+    int n = nums.size();
+
+    vector<vector<int>> ans = subsets2(nums);
+
+    for(auto i: ans){
+        cout<<"[";
+        for(auto j: i){
+
+            cout<<j<<" ";
+        }
+        cout<<"]"<<endl;
+    }
+
+    return 0;
+}
--- Day 9 Recursion/2.Get_All_Unique_Subsets.cpp
+++ Day 9 Recursion/2.Get_All_Unique_Subsets.cpp
@@ -3,11 +3,11 @@
 #include<iostream>
 #include<vector>
 #include<algorithm>
-#include<set>
 using namespace std;
 
+
 // O(2^N) for generating every subset and O(K) to insert every subset in another data structure
-// 1. brute - HashSet ? O(2^N(logN)*K) : O(2^N*K)
+// 1. brute - HashSet ? O(2^N * (2^N*(log2^N)) * K) : O(2^N*K) + O(2*N*K)
 void helper(int ind, vector < int > & nums, vector < int > temp, set < vector < int >> & st) {
     
     if(ind == nums.size()){
@@ -33,6 +33,41 @@ vector<vector<int>> subsets(vector<int>& nums){
     helper(0, nums, temp, st);
 
     for(auto vec: st) ans.push_back(vec);
+
+    return ans;
+}
+
+
+
+
+// O(2^n) for generating every subset and O(k)  to insert every subset in another data structure
+// optimal ? O(2^N(logN)*K) : O(2^N*K)
+void helper(int ind, vector<int>& nums, vector<int>& subset, vector<vector<int>>& ans, bool prev){
+
+    if(ind == nums.size()){
+
+        ans.push_back(subset);
+        return;
+    }
+    else{
+
+        helper(ind + 1, nums, subset, ans, false); // ith elem not picked
+        if(ind > 0 && nums[ind] == nums[ind-1] && !prev) return;
+
+        subset.push_back(nums[ind]); // ith elem picked
+        helper(ind + 1, nums, subset, ans, true);
+
+        subset.pop_back(); // backtracking
+    }
+}
+
+vector<vector<int>> subsets(vector<int>& nums){
+
+    vector<vector<int>> ans;
+
+    vector<int> subset;
+
+    helper(0, nums, subset, ans, false);
 
     return ans;
 }
